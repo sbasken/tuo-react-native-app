980ac8ea304ebdc21e2d6285706b8baf503a9eaa
Saki Basken <sakibasken@Sakis-MacBook-Pro.local>
2024-02-14 20:21:11 -0800

    Finish initial set up, add basic folders and basic app.js with NavigationContainer.

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Image 
+          source={require('./assets/logo.png')}
+          style={{width: 200, height: 200}}
+          resizeMode="contain"
+        />
+        <Text>Your Circadian Healthy Life Starts Here</Text>
+        <View style={styles.fixToText}>
+          <Button
+            title="Left button"
+            color="#7d55c7"
+            onPress={() => Alert.alert('Left button pressed')}
+          />
+          <Button
+            title="Right button"
+            color="#7d55c7"
+            onPress={() => Alert.alert('Right button pressed')}
+          />
+        </View>
+        
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +37,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#7d55c7',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
